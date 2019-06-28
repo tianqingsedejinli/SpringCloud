803bebe9650fb6c57245aae57c48881c3dc4d4e7
Hongyi <Hongyi@Hongyi>
2019-06-28 15:02:27 +0800

    加了注释

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,7 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
+	//±‡∫≈
 	private Integer id;
+	//”√ªß
 	private String name;
 	private Integer number;
 }
